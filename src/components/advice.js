08787fe91b30174b886c27beb8c54ef996a283dc
Robin Joshua Del Mundo <batbord94@gmail.com>
2022-04-11 06:09:17 +0800

    Added a landmark and fixed srcSet

--- src/components/advice.js
+++ src/components/advice.js
@@ -4,7 +4,7 @@ import divider from '../images/pattern-divider-desktop.svg';
 import mobileDivider from '../images/pattern-divider-mobile.svg';
 
 const FetchAdvice = (props) => {
-	
+
 	const [error, setError] = useState(null);
     const [isLoaded, setIsLoaded] = useState(false);
     const [slip, setSlip] = useState([]);
@@ -95,7 +95,7 @@ const Advice = () => {
 			<div className={'advice-wrapper' + newQuote}>
 				<FetchAdvice id={id} />
 				<div className="divider noselect">
-					<img src={mobileDivider} srcSet={`${mobileDivider} 600w, ${divider} 3200w, `} alt=""/>
+					<img src={mobileDivider} srcSet={`${mobileDivider} 600w, ${divider} 3200w `} alt=""/>
 				</div>
 			</div>
 			<div onClick={handleClick} className={'random-quote-button noselect' + clicked + preventClass}>
